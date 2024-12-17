6e146201fc6f9f8ad3e4dd5b05fdcc59979b2108
DhruvDhruvDhruvDhruv <DhruvDurv@gmail.com>
2024-12-17 02:22:27 +0000

    added dash, coyote time, a big gap to test jump on map, a bunch of sprite sheets to use in assets

--- slashplatformer/scripts/hero.gd
+++ slashplatformer/scripts/hero.gd
@@ -2,40 +2,85 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
+const DECELERATION = 100
 const JUMP_VELOCITY = -400.0
-@onready var hero_sprite: AnimatedSprite2D = $AnimatedSprite2D
+const DASH_VELOCITY = 1500.0
 
+@onready var hero_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var coyote_timer: Timer = $coyote_timer
+@onready var dash_timer: Timer = $dash_timer
+@onready var label: Label = $CanvasLayer/Label
+
+var coyote_allowed := true
+var dash_reset_allowed := true
+var timer_started := false
+var dashes := 1
+
+func reset_jump():
+	coyote_timer.stop()
+	timer_started = false
+	coyote_allowed = true
+
+func reset_dash():
+	dash_timer.stop()
+	dashes = 1
+
+func face_direction(mov_dir):
+	if mov_dir  > 0:
+		hero_sprite.flip_h = false
+	elif mov_dir < 0:
+		hero_sprite.flip_h = true
 
 func _physics_process(delta: float) -> void:
+	# DEBUG STATEMENT
+	label.text = "dash_reset_allowed: " + str(dash_reset_allowed) + "\nDashes: " + str(dashes)
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		if not timer_started:
+			coyote_timer.start()
+			timer_started = true
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and coyote_allowed:
 		velocity.y = JUMP_VELOCITY
-		
-
-	# Get the input direction and handle the movement/deceleration.
+	
+	var direction = -1 if hero_sprite.flip_h else 1
+	if Input.is_action_just_pressed("dash") and dashes > 0:
+		velocity.x = direction * DASH_VELOCITY
+		dash_timer.start()
+		dashes -= 1
+		hero_sprite.play("dash")
+		if velocity.y > 0:
+			velocity.y = -110
+		dash_reset_allowed = false
+			
+	# Get the input move_direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("move_left", "move_right")
-
+	var move_direction := Input.get_axis("move_left", "move_right")
+	face_direction(move_direction)
+	
 	if is_on_floor():
-		if direction == 0:
+		reset_jump()
+		if dash_reset_allowed:
+			reset_dash()
+		if move_direction == 0:
 			hero_sprite.play("idle")
 		else:
 			hero_sprite.play("run")
 	else:
 		hero_sprite.play("jump")
 
-	if direction:
-		hero_sprite.play("run")
-		velocity.x = direction * SPEED
-		if direction  > 0:
-			hero_sprite.flip_h = false
-		elif direction < 0:
-			hero_sprite.flip_h = true
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	velocity.x = move_toward(velocity.x, 0, DECELERATION)
+	if move_direction and dash_reset_allowed:
+		velocity.x = move_direction * SPEED 
+		
 
 	move_and_slide()
+
+
+func _on_coyote_timer_timeout() -> void:
+	coyote_allowed = false
+
+func _on_dash_timer_timeout() -> void:
+	dash_reset_allowed = true
